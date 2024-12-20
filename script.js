57da1048cc7ebdadbdc02c48d55e95ac8e509e47
Puja-me <177840100+Puja-me@users.noreply.github.com>
2024-12-20 10:36:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website loaded successfully.');
+  });
+  function backtotop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
